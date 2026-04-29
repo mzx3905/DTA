9d27180d6a14b7cab433ab02f963e1b3825a9d52
mzx3905 <qq377825093@gmail.com>
2026-04-29 22:13:55 +0800

    Update model.py

--- models/model.py
+++ models/model.py
@@ -14,9 +14,11 @@ class DTAModel(nn.Module):
     def forward(self, drug_graph, prot_feat, prot_mask):
         drug_feat, drug_mask = self.drug_encoder(drug_graph)
 
-        fused_feat, mask = self.interaction(
+        # 🔥 接收新增的全局蛋白特征
+        fused_drug, drug_mask, global_prot = self.interaction(
             drug_feat, drug_mask,
             prot_feat, prot_mask
         )
 
-        return self.predictor(fused_feat, mask)
+        # 🔥 把全局蛋白特征一起传给打分器
+        return self.predictor(fused_drug, drug_mask, global_prot)
